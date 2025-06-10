016faa8d989440b076088b8df14b42d9c2aeb5eb
Yaswanth Lakhanapuram <yaswanth2074@gmail.com>
2025-06-10 12:24:58 +0530

    Delete dbConnection.php

--- dbConnection.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    try {
-        $DSN = 'mysql:host=localhost;dbname=blog';
-        $conn = new PDO($DSN, "root", "");
-    } catch (Exception $e) {
-        echo "Exception is :: ".$e;
-    }
-?>
